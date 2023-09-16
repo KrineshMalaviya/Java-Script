85fe3b89ef20db252aa2ac3153486834f1f35817
Krinesh Malaviya <145100325+KrineshMalaviya@users.noreply.github.com>
2023-09-16 03:50:35 +0000

    Datatype

---
+++ 01_basics/02_Datatype.js
@@ -0,0 +1,25 @@
+
+console.log(3 
+      + 
+      3) // code readability shoulb be high
+
+//alert(3+3) //we are using nodejs, not browser      
+console.log("krinesh");
+
+let name = "krinesh";
+let age = 19;
+let isLoggedIn = false;
+
+//number =>  2 to power 53
+//bigint
+// string => ""
+// boolean => true/false 
+// null => standalone value
+// undefined 
+// symbol => unique
+
+// object 
+
+console.log(typeof "krinesh");
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
