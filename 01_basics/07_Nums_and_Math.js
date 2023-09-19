579f410afd750cb5732f7abc3d12407145ffdc76
Krinesh Malaviya <145100325+KrineshMalaviya@users.noreply.github.com>
2023-09-19 05:53:24 +0000

    Nums and Math

---
+++ 01_basics/07_Nums_and_Math.js
@@ -0,0 +1,42 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString("en-IN"));
+
+// *********************** Maths *****************************
+
+console.log(Math);
+console.log(Math.abs(-3));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2)); //conver to upper
+console.log(Math.floor(4.9)); //convert to lower
+console.log(Math.min(4,2,3,5));
+console.log(Math.max(4,2,3,5));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+// const min = 10
+// const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+// dies 1 to 6
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min );
+console.log(Math.floor(Math.random() * (max - min +1)) + min );
+
+//
